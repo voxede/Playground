0451b63a731ec4fbafd7e34fcf880dac1a5d8fbc
voxede <ivax3030@gmail.com>
2024-03-13 16:11:13 -0300

    more hud stuff

--- export/hl/haxe/ApplicationMain.hx
+++ export/hl/haxe/ApplicationMain.hx
@@ -35,7 +35,7 @@ class ApplicationMain
 		ManifestResources.init(config);
 		#end
 
-		app.meta["build"] = "22";
+		app.meta["build"] = "23";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "Playground";
 		app.meta["name"] = "Playground";
--- source/states/HUD.hx
+++ source/states/HUD.hx
@@ -11,6 +11,11 @@ import flixel.util.FlxColor;
 class HUD extends FlxSpriteGroup
 {
 	private var _textScore:FlxText;
+	private var _textTime:FlxText;
+	private var _textCoin:FlxText;
+	private var _textWorld:FlxText;
+
+	private var _iconCoin:FlxSprite;
 
 	static inline var OFFSET:Int = 4;
 
@@ -19,14 +24,40 @@ class HUD extends FlxSpriteGroup
 		super();
 
 		_textScore = new FlxText(OFFSET, OFFSET, 0);
-		add(_textScore);
+		_textCoin = new FlxText(FlxG.width * 0.33, OFFSET + 10, 0);
+		_textWorld = new FlxText(FlxG.width * 0.66, OFFSET, 0);
+		_textTime = new FlxText(OFFSET, OFFSET, FlxG.width - OFFSET * 2);
 
-		_textScore.scrollFactor = new FlxPoint(0, 0);
+		add(_textScore);
+		add(_textCoin);
+		add(_textWorld);
+		add(_textTime);
+
+		_textScore.alignment = FlxTextAlign.LEFT;
+		_textCoin.alignment = FlxTextAlign.CENTER;
+		_textWorld.alignment = FlxTextAlign.CENTER;
+		_textTime.alignment = FlxTextAlign.RIGHT;
+
+		_iconCoin = new FlxSprite(FlxG.width * 0.33 - 4, OFFSET + 10 + 4);
+		_iconCoin.loadGraphic("assets/hud.png", true, 8, 8);
+		_iconCoin.animation.add("coin", [0], 0);
+		_iconCoin.animation.play("icon");
+
+		add(_iconCoin);
+
+		forEach(function(member)
+		{
+			member.scrollFactor.set(0, 0);
+		}, false);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		_textScore.text = "SCORE\n" + StringTools.lpad(Std.string(Reg.score), "0", 5);
+		_textCoin.text = " x " + StringTools.lpad(Std.string(Reg.coins), "0", 2);
+		_textTime.text = "TIME\n" + StringTools.lpad(Std.string(Math.floor(Reg.time)), "0", 3);
+		_textWorld.text = "STAGE\n" + Reg.level;
+
 		super.update(elapsed);
 	}
 }
