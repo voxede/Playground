22a19d9379293664ba3edb76e0cdc8e07143eb67
voxede <ivax3030@gmail.com>
2024-03-13 15:49:30 -0300

    add hud class

---
+++ source/states/HUD.hx
@@ -0,0 +1,32 @@
+package states;
+
+import flixel.FlxBasic;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.group.FlxSpriteGroup;
+import flixel.math.FlxPoint;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+
+class HUD extends FlxSpriteGroup
+{
+	private var _textScore:FlxText;
+
+	static inline var OFFSET:Int = 4;
+
+	public function new()
+	{
+		super();
+
+		_textScore = new FlxText(OFFSET, OFFSET, 0);
+		add(_textScore);
+
+		_textScore.scrollFactor = new FlxPoint(0, 0);
+	}
+
+	override public function update(elapsed:Float)
+	{
+		_textScore.text = "SCORE\n" + (Reg.score);
+		super.update(elapsed);
+	}
+}
