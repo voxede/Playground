4d376250d0250c8473fdd78352a5e1fca2e0dfbb
voxede <ivax3030@gmail.com>
2024-03-13 12:32:34 -0300

    player class has been created

---
+++ source/Player.hx
@@ -0,0 +1,14 @@
+import flixel.FlxSprite;
+
+class Player extends FlxSprite
+{
+	public function new()
+	{
+		super();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
